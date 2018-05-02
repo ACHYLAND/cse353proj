ba8ef9a3c325380bb90d419e4f4ed723433ed432
Alicia <alicia.hyland@stonybrook.edu>
2018-05-02 17:16:59 -0400

    Create CSV reader

--- CSVReader.java
+++ CSVReader.java
@@ -2,7 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class CSVReader {
-    public void
+    public ArrayList<Vector> read(String filename) {
+            BufferedReader br;
+            String line;
+            final String COMMA_DELIMITER = ",";
+
+            try {
+                br = new BufferedReader(new FileReader(filename));
+                ArrayList<Vector> vectors = new ArrayList<>();
+
+                while ((line = br.readLine()) != null) {
+                    String[] fvs = (line.split(COMMA_DELIMITER));
+                    Vector<Integer> featureVector = new Vector<>(fvs.length);
+
+                    for (int i = 0; i < fvs.length; i++) {
+                        featureVector.set(i, Integer.parseInt(fvs[i]));
+                    }
+                    vectors.add(featureVector);
+
+                }
+
+                return vectors;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+    }
 }
